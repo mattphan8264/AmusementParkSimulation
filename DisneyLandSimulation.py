8a408adee91a1a6f982ca7ed3aa8bb8aaefb86eb
Matthew <mattphan8264@gmail.com>
2019-06-01 14:14:57 -0700

    Update DisneyLandSimulation.py

--- DisneyLandSimulation.py
+++ DisneyLandSimulation.py
@@ -7,9 +7,8 @@ import time
 #How big the grid will be
 GridWidth = 30
 GridHeight = 20
-grid = np.zeros((GridWidth, GridHeight)) - 1    #grid that holds position of rides
+grid = np.zeros((GridWidth, GridHeight)) + 7    #grid that holds position of rides
 walkGrid = np.zeros((GridWidth, GridHeight))    #grid that holds amount of people per acre
-
 
 #Ride information. Each index corresponds to a single ride (i.e. index 0 in ridelocation, ID, duration, and max
 #are for the same ride)
@@ -26,35 +25,50 @@ rideNames = ['Space Mountain', 'Indiana Jones Adventure', 'Matterhorn Bobsleds',
             'The Many Adventures of Winnie the Pooh', 'Alice in Wonderland', 'Mad Tea Party', 'Astro Orbiter', 'Dumbo the Flying Elephant', 'Buzz Lightyear Astro Blasters', 
             'King Arthur Carrousel', 'Mark Twain Riverboat', 'Gadgets Go Coaster', 'Snow Whites Scary Adventures', 'Mr. Toads Wild Ride', 'Pinocchios Daring Journey', 'Sailing Ship Columbia'
             'Casey Jr. Circus Train', 'Roger Rabbits Car Toon Spin', 'Davy Crocketts Explorer Canoes']
+nodes = [0, 1, 2, 3, 4, 5, 6]
+nodeConnections = [[1], [0, 2, 4, 5], [1, 3], [2], [1], [1, 6], [5]]
+rideNodeLocations = [3, 6, 2, 6, 6, 2, 2, 5, 5, 6, 4, 4, 2, 4, 5, 2, 2, 2, 4, 2, 4, 5, 4, 4, 4, 4, 5, 4, 4, 5]
+nodeLocations = [[17, 19], [17, 10], [23, 10], [27, 18], [17, 3], [3, 12], [10, 16]]
 
+rideAvailableTime = [[], [], [], [], [], [], [], [], [], [], 
+                     [], [], [], [], [], [], [], [], [], [], 
+                     [], [], [], [], [], [], [], [], [], []]
 fastPassRides = [0, 1, 2, 4, 6, 7, 8, 10, 19, 28]
 
-Hours = np.arange(16) * 20
-AttendeesPerHour = [300, 100, 0, 400, 100, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-maxAttendees = 1000
+AttendeesPerHour = [300, 100, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+maxAttendees = 500
+repeatedRides = False
 
 #Our focus group
-GroupDestinations = [0, 3, 2, 6, 8, 10, 15]
+GroupDestinations = [22, 27, 25]
 GroupAmount = 3
-TotalWalkSteps = 0
-MinWalkSteps = 0
 OptimizedDestination = GroupDestinations
 Entrance = [17, 19]
+nodeMovement = True
+TotalWalk = 0
+MinWalk = 0
 AverageRidesPerDay = 9
 Std = 1
-EntranceTime = 20
 
-ShuffleAmount = 5
-Repetitions = 10
-PlotOn = False
+ShuffleAmount = 1
+Repetitions = 1
+PlotOn = True
 
+#statistics
+TotalWalkStepArray = []
+TotalWaitStepArray = []
+TotalLeaveStepArray = []
+walkStepArray = []
+waitStepArray = []
+leaveStepArray = []
 
 #How big the park is
 DisneyLandAcres = 85
-DefaultWalkTime = 1
+DefaultWalkTime = 0
 WalkDelayAmount = 100   #How many people it takes to add delay to the group in a particular cell
 AmusementParkOpenTime = 320     #how long the park stays open
-MaxEnterAmount = 3  #how many people can enter the park at start
+MaxEnterAmount = 5  #how many people can enter the park at start
+time = 0
 
 GroupList = []
 AmusementRideList = []
@@ -69,12 +83,21 @@ LEAVING = 3
 
 #Represents a group
 class Group:
-    def __init__(self, ID, destinations, count):
+    def __init__(self, ID, destinations, rideIDs, count):
         self.ID = ID
         self.Count = count
         self.Destinations = destinations
+        self.RideIDList = rideIDs
+        self.node = 0
+        self.nextNode = 1
+        self.nextNodeDestination = nodeLocations[self.nextNode]
+        self.walkingTowardsNode = True
         self.FastPass = np.zeros(len(destinations))
         self.TotalSteps = 0
+        
+        self.totalWalkSteps = 0
+        self.totalWaitSteps = 0
+        self.totalLeaveSteps = 0
         
         #generating which rides have fast passes. Place holder until actual data is here
         for i in range(np.random.randint(len(destinations))):
@@ -85,60 +108,158 @@ class Group:
         self.CurrentDestination = 0
         self.Status = -1
         self.Location = [17, 19]
-        #Adding group member count to start position
-        walkGrid[self.Location[0]][self.Location[1]] += self.Count
+        
         self.WalkTime = 0
         
         self.NextDestination = ridelocation[destinations[0]]
     
+    
+    def calculateNextNode(self):
+        targetNodeInt = 0
+        if self.Status == WALKING:
+            targetNodeInt = self.RideIDList[self.CurrentDestination]
+                
+        if self.Status == WALKING or self.Status == LEAVING:
+            if targetNodeInt in nodeConnections[self.node]:
+                self.nextNode = targetNodeInt
+            elif len(nodeConnections[self.node]) == 1:
+                self.nextNode = nodeConnections[self.node][0]
+            else:
+                
+                for i in range(len(nodeConnections[self.node])):
+                    tempInt = nodeConnections[self.node][i]
+                    if targetNodeInt in nodeConnections[tempInt]:
+                        self.nextNode = tempInt
+                    else:
+                        for a in range(len(nodeConnections[tempInt])):
+                            nextTempInt = nodeConnections[tempInt][a]
+                            if (targetNodeInt in nodeConnections[nextTempInt]):
+                                self.nextNode = nextTempInt
+                            
+    
+    def calculateOpenRide(self):
+        if (len(self.Destinations) >= self.CurrentDestination):
+            return False
+        tempVal = self.Destinations[self.CurrentDestination]
+        if (len(rideAvailableTime[tempVal]) > 0):
+            tempArr = rideAvailableTime[tempVal]
+            for i in range(len(tempArr)):
+                if time > tempArr[i] and time < tempArr[i + 1]:
+                    difference = len(self.Destinations) - self.CurrentDestination
+                    randomVal = np.random.randint(0, difference)
+                    
+                    if (self.CurrentDestination + randomVal < len(self.Destinations)):
+                        swapVal = self.Destinations[self.CurrentDestination]
+                        self.Destinations[self.CurrentDestination] = self.Desinations[self.CurrentDestination + randomVal]
+                        self.Destinations[self.CurrentDestination + randomVal] = swapVal
+                        
+                        swapVal = self.RideIDList[self.CurrentDestination]
+                        self.RideIDList[self.CurrentDestination] = self.RideIDList[self.CurrentDestination + randomVal]
+                        self.RideIDList[self.CurrentDestination + randomVal] = swapVal
+                        
+                        self.NextDestination = ridelocation[self.Destinations[self.CurrentDestination]]
+
+                    return False
+                i += 1
+        return True
+        
     #Run when ride is done. If out of destinations, head towards exit    
     def rideFinished(self):
         self.CurrentDestination += 1
         if (self.CurrentDestination >= len(self.Destinations)):
             self.Status = LEAVING
+            self.walkingTowardsNode = True
         else:
             #start walking, set destination to next ride location
             self.Status = WALKING
-            self.NextDestination = ridelocation[self.CurrentDestination]
+            self.NextDestination = ridelocation[self.Destinations[self.CurrentDestination]]
+            if self.RideIDList[self.CurrentDestination] != self.node:
+                self.calculateNextNode()
+                self.walkingTowardsNode = True
+            
+    def freeWalk(self):
+            #If leaving, head towards 0 0
+        if (self.Status == LEAVING):
+            if (self.Location[0] > Entrance[0]):
+                self.Location[0] -= 1
+            elif (self.Location[0] < Entrance[0]):
+                self.Location[0] += 1
+            elif (self.Location[1] > Entrance[1]):
+                self.Location[1] -= 1
+            elif (self.Location[1] < Entrance[1]):
+                self.Location[1] += 1
+            #If walking, walk towards destination
+        elif (self.Status == WALKING):
+            if (self.Location[0] > self.NextDestination[0]):
+                self.Location[0] -= 1
+            elif (self.Location[0] < self.NextDestination[0]):
+                self.Location[0] += 1
+            elif (self.Location[1] > self.NextDestination[1]):
+                self.Location[1] -= 1
+            elif (self.Location[1] < self.NextDestination[1]):
+                self.Location[1] += 1
+            elif (self.Location == self.NextDestination):
+                #If arrived at destination, set to waiting and join the line of the ride
+                self.Status = WAITING
+                if (self.FastPass[self.CurrentDestination] == 1):
+                    AmusementRideList[self.Destinations[self.CurrentDestination]].groupLinedUpFastPass(self.ID, self.Count)
+                else:
+                    AmusementRideList[self.Destinations[self.CurrentDestination]].groupLinedUp(self.ID, self.Count)
+                
+    def nodeAssistWalk(self):
+        if (self.Location[0] > nodeLocations[self.nextNode][0]):
+            self.Location[0] -= 1
+        elif (self.Location[0] < nodeLocations[self.nextNode][0]):
+            self.Location[0] += 1
+        elif (self.Location[1] > nodeLocations[self.nextNode][1]):
+            self.Location[1] -= 1
+        elif (self.Location[1] < nodeLocations[self.nextNode][1]):
+            self.Location[1] += 1
+                    
+    def nodeWalk(self):
+        if (self.Status == WAITING):
+            pass
+        if self.Location == nodeLocations[self.nextNode]:
+            self.node = self.nextNode
+            if self.Status != LEAVING:
+                if self.RideIDList[self.CurrentDestination] == self.node:
+                    self.walkingTowardsNode = False
+                    self.freeWalk()
+                else:
+                    self.calculateNextNode()
+            else:
+                self.calculateNextNode()
+        else:
+            self.nodeAssistWalk()
        
     #Method for walking     
     def walk(self):
+        
         #If leaving and at exit, dont move anymore
         if (self.Status == LEAVING):
-            if (self.Location == Entrance):
-                return
+            if (self.Location == [17,19]):
+                return False
         elif (self.Status == WALKING or self.Status == WAITING):
             if (self.ID == 0):
                 self.TotalSteps += 1
+                
+        if (self.Status == WALKING):
+            self.totalWalkSteps += 1
+        elif (self.Status == WAITING):
+            self.totalWaitSteps += 1
+        elif (self.Status == LEAVING):
+            self.totalLeaveSteps += 1
         self.WalkTime -= 1
         
         #If can walk, walk
         if (self.WalkTime <= 0):
+            self.calculateOpenRide()
             walkGrid[self.Location[0]][self.Location[1]] -= self.Count
-            #If leaving, head towards 0 0
-            if (self.Status == LEAVING):
-                if (self.Location[0] > Entrance[0]):
-                    self.Location[0] -= 1
-                elif (self.Location[0] < Entrance[0]):
-                    self.Location[0] += 1
-                elif (self.Location[1] > Entrance[1]):
-                    self.Location[1] -= 1
-                elif (self.Location[1] < Entrance[1]):
-                    self.Location[1] += 1
-            #If walking, walk towards destination
-            elif (self.Status == WALKING):
-                if (self.Location[0] > self.NextDestination[0]):
-                    self.Location[0] -= 1
-                elif (self.Location[0] < self.NextDestination[0]):
-                    self.Location[0] += 1
-                elif (self.Location[1] > self.NextDestination[1]):
-                    self.Location[1] -= 1
-                elif (self.Location[1] < self.NextDestination[1]):
-                    self.Location[1] += 1
-                elif (self.Location == self.NextDestination):
-                    #If arrived at destination, set to waiting and join the line of the ride
-                    self.Status = WAITING
-                    AmusementRideList[self.Destinations[self.CurrentDestination]].groupLinedUp(self.ID, self.Count)
+            
+            if self.walkingTowardsNode == True and nodeMovement == True:
+                self.nodeWalk()
+            else:
+                self.freeWalk()
                     
             walkGrid[self.Location[0]][self.Location[1]] += self.Count
             #If we moved, set walk time to new calculation
@@ -229,30 +350,46 @@ class AmusementRide:
 #Initialize the grid and add amusement park rides to grid
 def setUpGrid():    
     for i in range(len(ridelocation)):
-          grid[ridelocation[i][0]][ridelocation[i][1]] = rideID[i]
+          #grid[ridelocation[i][0]][ridelocation[i][1]] = rideID[i]
+          grid[ridelocation[i][0]][ridelocation[i][1]] = rideNodeLocations[i]
           AmusementRideList.append(AmusementRide(rideDuration[i], rideMax[i]))
+    for i in range(len(nodeLocations)):
+        grid[nodeLocations[i][0]][nodeLocations[i][1]] = i
           
 #Initialize the group, initialize our focus group at index 0   
 def initGroups():  
-    GroupList.append(Group(0, GroupDestinations, GroupAmount)) 
+    nodeDestinations = []
+    for a in range(len(GroupDestinations)):
+        nodeDestinations.append(rideNodeLocations[GroupDestinations[a]])
+    GroupList.append(Group(0, GroupDestinations, nodeDestinations, GroupAmount)) 
             
     for i in range(1, maxAttendees):
         destinations = []
+        nodeDestinations = []
         
         rideCount = 0
         randomRideAmount = int(np.random.normal(AverageRidesPerDay, Std, 1))
         if (randomRideAmount < 1):
             randomRideAmount = 1
-        while (rideCount < randomRideAmount):
-            rideChosen = np.random.choice(rideID, p = ridePercentages)
-            if rideChosen in destinations:
-                if np.random.randint(10) < 1:
-                    destinations.append(rideChosen)
-                    rideCount += 1
-            else:
+            
+        if repeatedRides == True:
+            rideChosen = np.random.randint(0, 30)
+            for z in range(randomRideAmount):
                 destinations.append(rideChosen)
-                rideCount += 1
-        GroupList.append(Group(i, destinations, np.random.randint(1, 6)))
+                nodeDestinations.append(rideNodeLocations[rideChosen])
+        else:
+            while (rideCount < randomRideAmount):
+                rideChosen = np.random.choice(rideID, p = ridePercentages)
+                if rideChosen in destinations:
+                    if np.random.randint(10) < 1:
+                        destinations.append(rideChosen)
+                        nodeDestinations.append(rideNodeLocations[rideChosen])
+                        rideCount += 1
+                else:
+                    destinations.append(rideChosen)
+                    nodeDestinations.append(rideNodeLocations[rideChosen])
+                    rideCount += 1
+        GroupList.append(Group(i, destinations, nodeDestinations, np.random.randint(1, 6)))
     
 
 for shuffle in range(ShuffleAmount):
@@ -265,27 +402,25 @@ for shuffle in range(ShuffleAmount):
         
         CurrentEntered = 0
         for i in range(AmusementParkOpenTime):
+            time = i
             CurrentEntered += MaxEnterAmount
             Divisor = int(i / 20)
             
             if (CurrentEntered > AttendeesPerHour[Divisor]):
                 CurrentEntered = AttendeesPerHour[Divisor]
-            
+                
             if (CurrentEntered > len(GroupList)):
                 CurrentEntered = len(GroupList)
-               
-            if ( i >= EntranceTime):
-                if (GroupList[0].Status == -1):
-                    GroupList[0].Status = 0
-                GroupList[0].walk()
-            for j in range(1, CurrentEntered):
-                if (GroupList[j].Status == -1):
-                    GroupList[j].Status = 0
-                GroupList[j].walk()
+                
+            for i in range(CurrentEntered):
+                if (GroupList[i].Status == -1):
+                    GroupList[i].Status = 0
+                    walkGrid[GroupList[i].Location[0]][GroupList[i].Location[1]] += GroupList[i].Count
+                GroupList[i].walk()
             
-            for k in range(len(AmusementRideList)):
-                AmusementRideList[k].Ride()
-                AmusementRideList[k].FastPassRide()
+            for i in range(len(AmusementRideList)):
+                AmusementRideList[i].Ride()
+                AmusementRideList[i].FastPassRide()
             
             #Plot the graphs
             #1st graph is for ride locations
@@ -303,15 +438,25 @@ for shuffle in range(ShuffleAmount):
                 
                 plt.show()
                 plt.pause(.1)
+                
+        time = 0
         walkGrid = np.zeros((GridWidth, GridHeight))
-        TotalWalkSteps += GroupList[0].TotalSteps
+        TotalWalk += GroupList[0].TotalSteps
         GroupList = []
+        
+        walkStepArray.append(GroupList[0].totalWalkSteps)
+        waitStepArray.append(GroupList[0].totalWaitSteps)
+        leaveStepArray.append(GroupList[0].totalLeaveSteps)
+        
+    TotalWaitStepArray.append(waitStepArray)
+    TotalLeaveStepArray.append(leaveStepArray)
+    TotalWalkStepArray.append(walkStepArray)    
           
-    TotalWalkSteps = int(TotalWalkSteps / Repetitions)
-    if MinWalkSteps == 0 or TotalWalkSteps < MinWalkSteps:
-        MinWalkSteps = TotalWalkSteps
+    TotalWalk = int(TotalWalk / Repetitions)
+    if MinWalk == 0 or TotalWalk < MinWalk:
+        MinWalkSteps = TotalWalk
         OptimizedDestinations = GroupDestinations
-    TotalWalkSteps = 0
+    TotalWalk = 0
     
     np.random.shuffle(GroupDestinations)
     
